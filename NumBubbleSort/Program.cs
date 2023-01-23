354c8e74dc8804c75ed605977ba52b6a331d0dd7
Leila Schmidt <le.schmidt@douglas.de>
2023-01-23 10:01:05 +0100

    Created basic building blocks of program

--- NumBubbleSort/Program.cs
+++ NumBubbleSort/Program.cs
@@ -5,38 +5,18 @@
         static void Main(string[] args)
         {
             var numbers = new List<int>();
-            while (true)
+            bool noMoreNums = false;
+            while (noMoreNums == false)
             {
                 Console.WriteLine("Enter a number or 'stop' to end: ");
                 var input = Console.ReadLine();
-                if (input.ToLower() == "stop") break;
+                if (noMoreNums = true)
+                {
+                    break;
+                }
                 numbers.Add(int.Parse(input));
             }
-            Console.WriteLine("Original list: ");
-            foreach (var number in numbers)
-            {
-                Console.Write(number + " ");
-            }
-            Console.WriteLine();
 
-            for (int i = 0; i < numbers.Count - 1; i++)
-            {
-                for (int j = 0; j < numbers.Count - i - 1; j++)
-                {
-                    if (numbers[j] > numbers[j + 1])
-                    {
-                        var temp = numbers[j];
-                        numbers[j] = numbers[j + 1];
-                        numbers[j + 1] = temp;
-                    }
-                }
-            }
-            Console.WriteLine("Sorted list: ");
-            foreach (var number in numbers)
-            {
-                Console.Write(number + " ");
-            }
-            Console.WriteLine();
         }
     }
 }
