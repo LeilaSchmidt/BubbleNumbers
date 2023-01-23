bc2cb85f1bce89d74a81f962cbbf20047276e1b2
Leila Schmidt <lnsnani80@gmail.com>
2023-01-23 14:56:39 +0100

    added the outer for loop (nesting the inner one). The outer loop loops over the list itself, the inner one switches the positions and does the physical sorting.

--- NumBubbleSort/Program.cs
+++ NumBubbleSort/Program.cs
@@ -4,12 +4,12 @@
     {
         static void Main(string[] args)
         {
-            var numberList = new List<int>();
+            var numList = new List<int>();
             while (true)
             {
                 Console.WriteLine("Enter a number or 'stop' to end: ");
                 var userInput = Console.ReadLine();
-                if (userInput == "stop" || userInput == "Stop" || userInput == "STOP")
+                if (userInput.Trim().ToLower() == "stop")
                 {
                     break;
                 }
@@ -18,11 +18,27 @@
                     Console.WriteLine("This is not a number");
                     continue;
                 }
-                numberList.Add(numUserInput);
+                numList.Add(numUserInput);
             }
             Console.WriteLine("The numbers you entered were... ");
-            Console.WriteLine(String.Join(", ", numberList));
+            Console.WriteLine(String.Join(", ", numList));
+
+            for (int i = 0; i < numList.Count - 1; i++)
+            {
+                for (int j = 0; j < numList.Count - 1; j++)
+                {
+                    if (numList[j] > numList[j + 1])
+                    {
+                        var biggerNumber = numList[j];
+                        numList[j] = numList[j + 1];
+                        //var justChecking = numList[j];
+                        numList[j + 1] = biggerNumber;
+                    }
+                    Console.WriteLine(String.Join(", ", numList));
+                }
+                Console.WriteLine(String.Join(", ", numList));
+            }
+
         }
-        
     }
 }
