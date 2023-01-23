124ac4fac90eb0fede080b92b75b772aa8280746
Leila Schmidt <lnsnani80@gmail.com>
2023-01-23 15:02:28 +0100

    built the same program but while utilizing a while.

--- NumBubbleSort/Program.cs
+++ NumBubbleSort/Program.cs
@@ -23,9 +23,11 @@
             Console.WriteLine("The numbers you entered were... ");
             Console.WriteLine(String.Join(", ", numList));
 
-            for (int i = 0; i < numList.Count - 1; i++)
+            int i = 0;
+            while (i < numList.Count - 1)
             {
-                for (int j = 0; j < numList.Count - 1; j++)
+                int j = 0;
+                while (j < numList.Count - 1)
                 {
                     if (numList[j] > numList[j + 1])
                     {
@@ -33,10 +35,12 @@
                         numList[j] = numList[j + 1];
                         numList[j + 1] = biggerNumber;
                     }
-                    Console.WriteLine(String.Join(", ", numList));
+                    j++;
                 }
-                Console.WriteLine(String.Join(", ", numList));
+                i++;
             }
+            Console.WriteLine("The numbers you entered in ascending order are");
+            Console.WriteLine(String.Join(", ", numList));
 
         }
     }
